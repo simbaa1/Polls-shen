d4e44632a6f3f6f36c414d3aa7679c7c8c0a4e79
Chris Simba <simbakriss@gmail.com>
2019-10-21 06:47:44 +0300

    add all files

---
+++ polls/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+#from .views import polls_list, polls_detail
+from .apiviews import ChoiceList, CreateVote, PollViewSet, UserCreate, LoginView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('polls', PollViewSet, base_name='polls')
+
+urlpatterns = [
+    # path("polls/", PollList.as_view(), name="polls_list"),
+    # path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path("users/", UserCreate.as_view(), name="user_create"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/",
+         CreateVote.as_view(), name="create_vote")
+]
+
+urlpatterns += router.urls
